98c9ff77256a61726c1a5c66ced05013eb068659
Masego Mashishi <masegomashishi@gmail.com>
2025-09-28 11:19:54 +0200

    Added full project code

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("orderForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  // Simple redirect to thank you page
+  window.location.href = "thankyou.html";
+});
